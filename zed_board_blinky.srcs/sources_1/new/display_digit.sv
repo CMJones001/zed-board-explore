b04749677e87bc2dedc287a1193b9a9d902cc4c6
Carl Jones <c.m.jones001@gmail.com>
2024-07-27 21:11:02 +0200

    Add duty cycle dimming to digit

--- zed_board_blinky.srcs/sources_1/new/display_digit.sv
+++ zed_board_blinky.srcs/sources_1/new/display_digit.sv
@@ -19,5 +19,51 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+module display_digit(
+    input clk,
+    input [3:0] seven_seg_value,
+    input digit_select,
+    output reg [7:0] digit
+    );
+  
+    reg [7:0] digit_out_arr;
+    reg [7:0] select_array;
 
+    assign digit = digit_out_arr | select_array;
 
+    always @(posedge clk) begin
+          // We write the digit_out array in the form x0xx_xxxx
+          // this controls the selected bits. 0 is on for a segment.
+          // Segment layout:
+          //  2
+          // 3 5
+          //  7
+          // 0 4
+          //  1
+        case (seven_seg_value)
+          0:  digit_out_arr <= 8'b1000_0000;
+          1:  digit_out_arr <= 8'b1000_1111;
+          2:  digit_out_arr <= 8'b0001_1000;
+          3:  digit_out_arr <= 8'b0000_1001;
+          4:  digit_out_arr <= 8'b0000_0111;
+          5:  digit_out_arr <= 8'b0010_0001;
+          6:  digit_out_arr <= 8'b0010_0000;
+          7:  digit_out_arr <= 8'b1000_1011;
+          8:  digit_out_arr <= 8'b0000_0000;
+          9:  digit_out_arr <= 8'b0000_0001;
+         10:  digit_out_arr <= 8'b0000_0010;
+         11:  digit_out_arr <= 8'b0010_0100;
+         12:  digit_out_arr <= 8'b1011_0000;
+         13:  digit_out_arr <= 8'b0000_1100;
+         14:  digit_out_arr <= 8'b0011_0000;
+         15:  digit_out_arr <= 8'b0011_0010;
+         default: digit_out_arr <= 8'b1011_1111;
+        endcase
+        
+        // Bit-6 is used to choose the digit
+        case (digit_select)
+          0:  select_array <= 8'b0000_0000;
+          1:  select_array <= 8'b0100_0000;
+        endcase
+    end
+endmodule
--- zed_board_blinky.srcs/sources_1/new/top.v
+++ zed_board_blinky.srcs/sources_1/new/top.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company:
-// Engineer:
+// Engineer: Carl Jones
 //
 // Create Date: 05/17/2024 05:49:59 PM
 // Design Name:
 // Module Name: blinky
 // Project Name:
-// Target Devices:
+// Target Devices: ZedBoard
 // Tool Versions:
 // Description:
 //
@@ -26,14 +26,13 @@ module blinky
     input reg [7:0] SW,
     input reg BTNC,
     output reg [7:0] LED,
-    output reg [7:0] JC,
+    output [7:0] JC,
     output reg [7:0] JD
     );
 
     parameter int max_length = 26;
 
     reg [max_length:0] count = 0;
-    reg [7:0] led_out = 0;
     
     reg [3:0] data;
     reg [7:0] stored_sw;
@@ -42,25 +41,47 @@ module blinky
     parameter ssd_switch_bit = 15;
     reg digit_select;
     
+    parameter int duty_bits = 10; // 1024 counts
+    parameter int duty_on = 1024 * 0.2; // ~ 20% duty
+    
+    // Set a disabling mask on the SSD that turns off the display 
+    // based on the duty cycle.
+    reg [7:0] ssd_enable_mask;
+   
+    always @(posedge(GCLK)) begin
+        // The display is high-off, so set all bits high, but don't mess with the 
+        // digit select bit. 
+        if (count[duty_bits-1:0] < duty_on) ssd_enable_mask = 8'h00;
+        else ssd_enable_mask = 8'b1011_1111;
+    end
+    
+    reg [7:0] ssd_c_out;
+    assign JC = ssd_c_out | ssd_enable_mask;
+    reg [7:0] ssd_d_out;
+    assign JD = ssd_d_out | ssd_enable_mask; // disabling masking here;
+    
+    
     display_digit ssd_one_mod(
         .clk(GCLK),
         .seven_seg_value(data),
         .digit_select(digit_select),
-        .digit(JC)
+        .digit(ssd_c_out)
     );
     
     display_digit ssd_two_mod(
         .clk(GCLK),
         .seven_seg_value(stored_data),
         .digit_select(digit_select),
-        .digit(JD)
+        .digit(ssd_d_out)
     );
-    
+
+    // Store data
     always @(posedge(GCLK)) begin
        if (BTNC) stored_sw <= SW;
        else stored_sw <= stored_sw;
     end
     
+    // Swap between the digits
     always @(posedge(GCLK)) begin 
         case (count[ssd_switch_bit])
             0 : begin
@@ -77,8 +98,26 @@ module blinky
     end
 
     always @(posedge(GCLK)) count <= count + 1;
-    always @(*) begin
-        case({ count[max_length], count[max_length-1], count[max_length-2] })
+
+    LED_roller #(.count_length(max_length)) roller(
+        .clk(GCLK),
+        .count(count),
+        .LED(LED)
+    );
+    
+endmodule
+
+module LED_roller
+#(parameter count_length = 26)
+(
+    input reg clk,
+    input reg [count_length:0] count,
+    output reg [7:0] LED
+);
+    reg [7:0] led_out = 0;
+        
+    always @(posedge(clk)) begin
+        case({ count[count_length], count[count_length-1], count[count_length-2] })
             3'b000 : led_out = 8'b00000001;
             3'b001 : led_out = 8'b00000010;
             3'b010 : led_out = 8'b00000100;
@@ -92,58 +131,7 @@ module blinky
         endcase
     end
 
-    always @(posedge(GCLK)) begin
-        // {LD0, LD1, LD2, LD3, LD4, LD5, LD6, LD7} = led_out;
+    always @(posedge(clk)) begin
         LED <= led_out;
     end
 endmodule
-
-module display_digit(
-    input clk,
-    input [3:0] seven_seg_value,
-    input digit_select,
-    output reg [7:0] digit
-    );
-  
-    reg [7:0] digit_out_arr;
-    reg [7:0] select_array;
-
-    assign digit = digit_out_arr | select_array;
-
-    always @(posedge clk) begin
-          // We write the digit_out array in the form x0xx_xxxx
-          // this controls the selected bits. 0 is on for a segment.
-          // Segment layout:
-          //  2
-          // 3 5
-          //  7
-          // 0 4
-          //  1
-        case (seven_seg_value)
-          0:  digit_out_arr <= 8'b1000_0000;
-          1:  digit_out_arr <= 8'b1000_1111;
-          2:  digit_out_arr <= 8'b0001_1000;
-          3:  digit_out_arr <= 8'b0000_1001;
-          4:  digit_out_arr <= 8'b0000_0111;
-          5:  digit_out_arr <= 8'b0010_0001;
-          6:  digit_out_arr <= 8'b0010_0000;
-          7:  digit_out_arr <= 8'b1000_1011;
-          8:  digit_out_arr <= 8'b0000_0000;
-          9:  digit_out_arr <= 8'b0000_0001;
-         10:  digit_out_arr <= 8'b0000_0010;
-         11:  digit_out_arr <= 8'b0010_0100;
-         12:  digit_out_arr <= 8'b1011_0000;
-         13:  digit_out_arr <= 8'b0000_1100;
-         14:  digit_out_arr <= 8'b0011_0000;
-         15:  digit_out_arr <= 8'b0011_0010;
-         default: digit_out_arr <= 8'b1011_1111;
-        endcase
-        
-        // Bit-6 is used to choose the digit
-        case (digit_select)
-          0:  select_array <= 8'b0000_0000;
-          1:  select_array <= 8'b0100_0000;
-        endcase
-    end
-endmodule
-
