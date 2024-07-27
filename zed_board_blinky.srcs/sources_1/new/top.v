27177b86dcb64577569efcf7141990f927fb57ca
Carl Jones <c.m.jones001@gmail.com>
2024-07-27 21:32:55 +0200

    Refactoring and comment tidying

--- zed_board_blinky.srcs/sources_1/new/top.v
+++ zed_board_blinky.srcs/sources_1/new/top.v
@@ -33,78 +33,78 @@ module blinky
     parameter int max_length = 26;
 
     reg [max_length:0] count = 0;
-    
-    reg [3:0] data;
-    reg [7:0] stored_sw;
-    reg [3:0] stored_data;
+
+    reg [3:0] digit_one;
+    reg [3:0] digit_two;
+    reg [7:0] stored_switches;
 
     parameter ssd_switch_bit = 15;
     reg digit_select;
-    
+
     parameter int duty_bits = 10; // 1024 counts
-    parameter int duty_on = 1024 * 0.2; // ~ 20% duty
-    
-    // Set a disabling mask on the SSD that turns off the display 
+    parameter int duty_on = 2 ** duty_bits * 0.2; // ~ 20% duty
+
+    // Set a disabling mask on the SSD that turns off the display
     // based on the duty cycle.
     reg [7:0] ssd_enable_mask;
-   
+
     always @(posedge(GCLK)) begin
-        // The display is high-off, so set all bits high, but don't mess with the 
-        // digit select bit. 
+        // The display is high-off, so set all bits high, but don't mess with the
+        // digit select bit.
         if (count[duty_bits-1:0] < duty_on) ssd_enable_mask = 8'h00;
         else ssd_enable_mask = 8'b1011_1111;
     end
-    
+
     reg [7:0] ssd_c_out;
     assign JC = ssd_c_out | ssd_enable_mask;
     reg [7:0] ssd_d_out;
-    assign JD = ssd_d_out | ssd_enable_mask; // disabling masking here;
-    
-    
-    display_digit ssd_one_mod(
+    assign JD = ssd_d_out | ssd_enable_mask;
+
+    // Generete the 7-segment display output for each PMOD
+    display_digit(
         .clk(GCLK),
-        .seven_seg_value(data),
+        .seven_seg_value(digit_one),
         .digit_select(digit_select),
         .digit(ssd_c_out)
     );
-    
-    display_digit ssd_two_mod(
+
+    display_digit(
         .clk(GCLK),
-        .seven_seg_value(stored_data),
+        .seven_seg_value(digit_two),
         .digit_select(digit_select),
         .digit(ssd_d_out)
     );
 
-    // Store data
+    // Store data on pressing the middle button
     always @(posedge(GCLK)) begin
-       if (BTNC) stored_sw <= SW;
-       else stored_sw <= stored_sw;
+       if (BTNC) stored_switches <= SW;
+       else stored_switches <= stored_switches;
     end
-    
+
     // Swap between the digits
-    always @(posedge(GCLK)) begin 
+    always @(posedge(GCLK)) begin
         case (count[ssd_switch_bit])
             0 : begin
                 digit_select <= 0;
-                data <=  SW[3:0];
-                stored_data <= stored_sw[3:0];
+                digit_one <=  SW[3:0];
+                digit_two <= stored_switches[3:0];
             end
             1 : begin
                 digit_select <= 1;
-                data <=  SW[7:4];
-                stored_data <= stored_sw[7:4];
+                digit_one <=  SW[7:4];
+                digit_two <= stored_switches[7:4];
             end
         endcase
     end
 
     always @(posedge(GCLK)) count <= count + 1;
 
-    LED_roller #(.count_length(max_length)) roller(
+    LED_roller #(.count_length(max_length)) (
         .clk(GCLK),
         .count(count),
         .LED(LED)
     );
-    
+
 endmodule
 
 module LED_roller
@@ -115,7 +115,7 @@ module LED_roller
     output reg [7:0] LED
 );
     reg [7:0] led_out = 0;
-        
+
     always @(posedge(clk)) begin
         case({ count[count_length], count[count_length-1], count[count_length-2] })
             3'b000 : led_out = 8'b00000001;
