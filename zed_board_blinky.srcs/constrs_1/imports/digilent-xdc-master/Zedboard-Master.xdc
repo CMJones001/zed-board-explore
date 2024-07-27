f1983bb9d36d360676c3b88076ca62cd8cced450
Carl Jones <c.m.jones001@gmail.com>
2024-07-27 18:01:21 +0200

    Digit store

--- zed_board_blinky.srcs/constrs_1/imports/digilent-xdc-master/Zedboard-Master.xdc
+++ zed_board_blinky.srcs/constrs_1/imports/digilent-xdc-master/Zedboard-Master.xdc
@@ -121,14 +121,14 @@ set_property PACKAGE_PIN T4  [get_ports {JC[7]}];  # "JC4_P"
 # ----------------------------------------------------------------------------
 # JD Pmod - Bank 13
 # ----------------------------------------------------------------------------
-#set_property PACKAGE_PIN W7 [get_ports {JD1_N}];  # "JD1_N"
-#set_property PACKAGE_PIN V7 [get_ports {JD1_P}];  # "JD1_P"
-#set_property PACKAGE_PIN V4 [get_ports {JD2_N}];  # "JD2_N"
-#set_property PACKAGE_PIN V5 [get_ports {JD2_P}];  # "JD2_P"
-#set_property PACKAGE_PIN W5 [get_ports {JD3_N}];  # "JD3_N"
-#set_property PACKAGE_PIN W6 [get_ports {JD3_P}];  # "JD3_P"
-#set_property PACKAGE_PIN U5 [get_ports {JD4_N}];  # "JD4_N"
-#set_property PACKAGE_PIN U6 [get_ports {JD4_P}];  # "JD4_P"
+set_property PACKAGE_PIN W7 [get_ports {JD[0]}];  # "JD1_N"
+set_property PACKAGE_PIN V7 [get_ports {JD[1]}];  # "JD1_P"
+set_property PACKAGE_PIN V4 [get_ports {JD[2]}];  # "JD2_N"
+set_property PACKAGE_PIN V5 [get_ports {JD[3]}];  # "JD2_P"
+set_property PACKAGE_PIN W5 [get_ports {JD[4]}];  # "JD3_N"
+set_property PACKAGE_PIN W6 [get_ports {JD[5]}];  # "JD3_P"
+set_property PACKAGE_PIN U5 [get_ports {JD[6]}];  # "JD4_N"
+set_property PACKAGE_PIN U6 [get_ports {JD[7]}];  # "JD4_P"
 
 # ----------------------------------------------------------------------------
 # OLED Display - Bank 13
@@ -172,14 +172,14 @@ set_property PACKAGE_PIN T4  [get_ports {JC[7]}];  # "JC4_P"
 # ----------------------------------------------------------------------------
 # User LEDs - Bank 33
 # ----------------------------------------------------------------------------
-set_property PACKAGE_PIN T22 [get_ports {LD0}];  # "LD0"
-set_property PACKAGE_PIN T21 [get_ports {LD1}];  # "LD1"
-set_property PACKAGE_PIN U22 [get_ports {LD2}];  # "LD2"
-set_property PACKAGE_PIN U21 [get_ports {LD3}];  # "LD3"
-set_property PACKAGE_PIN V22 [get_ports {LD4}];  # "LD4"
-set_property PACKAGE_PIN W22 [get_ports {LD5}];  # "LD5"
-set_property PACKAGE_PIN U19 [get_ports {LD6}];  # "LD6"
-set_property PACKAGE_PIN U14 [get_ports {LD7}];  # "LD7"
+set_property PACKAGE_PIN T22 [get_ports {LED[0]}];  # "LD0"
+set_property PACKAGE_PIN T21 [get_ports {LED[1]}];  # "LD1"
+set_property PACKAGE_PIN U22 [get_ports {LED[2]}];  # "LD2"
+set_property PACKAGE_PIN U21 [get_ports {LED[3]}];  # "LD3"
+set_property PACKAGE_PIN V22 [get_ports {LED[4]}];  # "LD4"
+set_property PACKAGE_PIN W22 [get_ports {LED[5]}];  # "LD5"
+set_property PACKAGE_PIN U19 [get_ports {LED[6]}];  # "LD6"
+set_property PACKAGE_PIN U14 [get_ports {LED[7]}];  # "LD7"
 
 # ----------------------------------------------------------------------------
 # VGA Output - Bank 33
@@ -202,7 +202,7 @@ set_property PACKAGE_PIN U14 [get_ports {LD7}];  # "LD7"
 # ----------------------------------------------------------------------------
 # User Push Buttons - Bank 34
 # ----------------------------------------------------------------------------
-#set_property PACKAGE_PIN P16 [get_ports {BTNC}];  # "BTNC"
+set_property PACKAGE_PIN P16 [get_ports {BTNC}];  # "BTNC"
 #set_property PACKAGE_PIN R16 [get_ports {BTND}];  # "BTND"
 #set_property PACKAGE_PIN N15 [get_ports {BTNL}];  # "BTNL"
 #set_property PACKAGE_PIN R18 [get_ports {BTNR}];  # "BTNR"
--- zed_board_blinky.srcs/sources_1/new/top.v
+++ zed_board_blinky.srcs/sources_1/new/top.v
@@ -24,72 +24,77 @@ module blinky
 (
     input GCLK,
     input reg [7:0] SW,
-    output reg LD0,
-    output reg LD1,
-    output reg LD2,
-    output reg LD3,
-    output reg LD4,
-    output reg LD5,
-    output reg LD6,
-    output reg LD7,
-    output reg [7:0] JC
+    input reg BTNC,
+    output reg [7:0] LED,
+    output reg [7:0] JC,
+    output reg [7:0] JD
     );
 
     parameter int max_length = 26;
 
     reg [max_length:0] count = 0;
-    reg [7:0] temp = 0;
+    reg [7:0] led_out = 0;
     
-    wire [3:0] data_one;
-    wire [3:0] data_two;
     reg [3:0] data;
-    
-    assign data_one = SW[3:0];
-    assign data_two = SW[7:4];
+    reg [7:0] stored_sw;
+    reg [3:0] stored_data;
 
+    parameter ssd_switch_bit = 15;
     reg digit_select;
-    
-    reg [7:0] ssd_one_out;
-    reg [7:0] ssd_two_out;
     
     display_digit ssd_one_mod(
         .clk(GCLK),
-        .seven_seg_value(data_one),
-        .digit_select(1),
-        .digit(ssd_one_out)
-    );
-    display_digit ssd_two_mod(
-        .clk(GCLK),
-        .seven_seg_value(data_two),
-        .digit_select(0),
-        .digit(ssd_two_out)
+        .seven_seg_value(data),
+        .digit_select(digit_select),
+        .digit(JC)
     );
     
+    display_digit ssd_two_mod(
+        .clk(GCLK),
+        .seven_seg_value(stored_data),
+        .digit_select(digit_select),
+        .digit(JD)
+    );
+    
+    always @(posedge(GCLK)) begin
+       if (BTNC) stored_sw <= SW;
+       else stored_sw <= stored_sw;
+    end
+    
     always @(posedge(GCLK)) begin 
-        case (count[15])
-            0 : JC <= ssd_one_out;
-            1 : JC <= ssd_two_out;
+        case (count[ssd_switch_bit])
+            0 : begin
+                digit_select <= 0;
+                data <=  SW[3:0];
+                stored_data <= stored_sw[3:0];
+            end
+            1 : begin
+                digit_select <= 1;
+                data <=  SW[7:4];
+                stored_data <= stored_sw[7:4];
+            end
         endcase
     end
 
     always @(posedge(GCLK)) count <= count + 1;
     always @(*) begin
         case({ count[max_length], count[max_length-1], count[max_length-2] })
-            3'b000 : temp = 8'b00000001;
-            3'b001 : temp = 8'b00000010;
-            3'b010 : temp = 8'b00000100;
-            3'b011 : temp = 8'b00001000;
-            3'b100 : temp = 8'b00010000;
-            3'b101 : temp = 8'b00100000;
-            3'b110 : temp = 8'b01000000;
-            3'b111 : temp = 8'b10000000;
+            3'b000 : led_out = 8'b00000001;
+            3'b001 : led_out = 8'b00000010;
+            3'b010 : led_out = 8'b00000100;
+            3'b011 : led_out = 8'b00001000;
+            3'b100 : led_out = 8'b00010000;
+            3'b101 : led_out = 8'b00100000;
+            3'b110 : led_out = 8'b01000000;
+            3'b111 : led_out = 8'b10000000;
 
-            default : temp = 8'b00000000;
+            default : led_out = 8'b00000000;
         endcase
     end
 
     always @(posedge(GCLK)) begin
-        {LD0, LD1, LD2, LD3, LD4, LD5, LD6, LD7} = temp;
+        // {LD0, LD1, LD2, LD3, LD4, LD5, LD6, LD7} = led_out;
+        LED <= led_out;
     end
 endmodule
 
@@ -99,35 +104,45 @@ module display_digit(
     input digit_select,
     output reg [7:0] digit
     );
-
-    reg [7:0] temp;
+  
+    reg [7:0] digit_out_arr;
     reg [7:0] select_array;
 
-    assign digit = temp | select_array;
+    assign digit = digit_out_arr | select_array;
 
     always @(posedge clk) begin
+          // We write the digit_out array in the form x0xx_xxxx
+          // this controls the selected bits. 0 is on for a segment.
+          // Segment layout:
+          //  2
+          // 3 5
+          //  7
+          // 0 4
+          //  1
         case (seven_seg_value)
-          0:  temp <= 8'b1000_0000;
-          1:  temp <= 8'b1000_1111;
-          2:  temp <= 8'b0001_1000;
-          3:  temp <= 8'b0000_1001;
-          4:  temp <= 8'b0000_0111;
-          5:  temp <= 8'b0010_0001;
-          6:  temp <= 8'b0010_0000;
-          7:  temp <= 8'b1000_1011;
-          8:  temp <= 8'b0000_0000;
-          9:  temp <= 8'b0000_0001;
-         10:  temp <= 8'b0000_0010;
-         11:  temp <= 8'b0010_0100;
-         12:  temp <= 8'b1011_0000;
-         13:  temp <= 8'b0000_1100;
-         14:  temp <= 8'b0011_0000;
-         15:  temp <= 8'b0011_0010;
-         default: temp <= 8'b1011_1111;
+          0:  digit_out_arr <= 8'b1000_0000;
+          1:  digit_out_arr <= 8'b1000_1111;
+          2:  digit_out_arr <= 8'b0001_1000;
+          3:  digit_out_arr <= 8'b0000_1001;
+          4:  digit_out_arr <= 8'b0000_0111;
+          5:  digit_out_arr <= 8'b0010_0001;
+          6:  digit_out_arr <= 8'b0010_0000;
+          7:  digit_out_arr <= 8'b1000_1011;
+          8:  digit_out_arr <= 8'b0000_0000;
+          9:  digit_out_arr <= 8'b0000_0001;
+         10:  digit_out_arr <= 8'b0000_0010;
+         11:  digit_out_arr <= 8'b0010_0100;
+         12:  digit_out_arr <= 8'b1011_0000;
+         13:  digit_out_arr <= 8'b0000_1100;
+         14:  digit_out_arr <= 8'b0011_0000;
+         15:  digit_out_arr <= 8'b0011_0010;
+         default: digit_out_arr <= 8'b1011_1111;
         endcase
+        
+        // Bit-6 is used to choose the digit
         case (digit_select)
-          0:  select_array <= 8'b0100_0000;
-          1:  select_array <= 8'b0000_0000;
+          0:  select_array <= 8'b0000_0000;
+          1:  select_array <= 8'b0100_0000;
         endcase
     end
 endmodule
